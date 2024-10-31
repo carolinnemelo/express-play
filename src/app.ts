87813256e9b071cd9fb5e4e4437c9353c10b56a8
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-10-31 09:59:24 +0100

    follow along

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,9 @@ export function createApp() {
   app.get("/status", (req, res) => {
     res.json({ status: "ready" });
   });
-//   const pancakesFeature = createPanc akesFeature();
-  app.get("/", (req, res) => {
-    res.json([]);
-  });
+  const pancakesFeature = createPancakesFeature();
+  app.use("/api/v1/pancakes", pancakesFeature.getRouter());
+  app.use("/api/v1/pankcakes", pancakesFeature.getRouter()); //ter duas rotas quando tem algum erro escrito pq vai quebrar pra todo mundo que ja usa o app
 
   return app;
 }
--- src/features/index.ts
+++ src/features/index.ts
@@ -4,6 +4,10 @@ export function createPancakesFeature() {
   return {
     getRouter() {
       const router = express.Router();
+      router.get("/", (req, res) => {
+        res.json(["sou uma panquequinha gostosa"]);
+      });
+      return router;
     },
   }; //all functions inside an object
 }
