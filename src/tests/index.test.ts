974ac81f785db87fabcc1ac4d920de915fbb261b
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-10-31 09:36:42 +0100

    move app to index file

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,10 +1,11 @@
 import { deepEqual } from "node:assert";
 import request  from "supertest";
 import test from "node:test";
-import express from "express";
+import { createApp } from ".";
 
 
-test("supertest works!", async () => {
+test("GET /status", async () => {
+  const app = createApp();  
 
   const result = await request(app).get("/status");
   deepEqual(result.status, 200)
