feddcec78a7258361359504bb38d32980940ee4b
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-07 09:31:43 +0100

    add .env and .env.exemple

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 
 import { createApp } from "./app";
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
+const db = drizzle(process.env.DATABASE_URL!);
 
 createApp().listen(3000, () => {
   console.log(`Server start on `);
 });
+
